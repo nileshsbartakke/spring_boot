eeedd0e7beebfe8f74d5378e03b3a51f92a81144
Nilesh Bartakke <36785993+nileshsbartakke@users.noreply.github.com>
2020-03-03 16:11:55 +0530

    Add files via upload

---
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/in28minute/microservices/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,105 @@
+package com.in28minute.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="Exchange_Value")  
+public class ExchangeValue {
+	
+	
+	@Id
+	@Column(name="id")  
+	private Long id;
+	
+	@Column(name="currency_from")
+	private String from;
+	
+	
+	@Column(name="currency_to")
+	private String to;
+	
+	
+	private BigDecimal conversionMultiple;
+	
+	@Column(name="port")
+	private int port;
+	
+	//default conatructor  
+	public ExchangeValue()  
+	{   
+		
+	}
+	
+	public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public String getFrom() {
+		return from;
+	}
+
+
+	public String getTo() {
+		return to;
+	}
+
+
+	public int getPort() {
+		return port;
+	}
+
+
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+
+
+
+	
+	
+	
+	
+
+}
