3833f19ed2d5d84a5f884c0c8f5cce8adda901bd
Nilesh Bartakke <36785993+nileshsbartakke@users.noreply.github.com>
2020-03-14 01:26:24 +0530

    Add files via upload

---
+++ Spring_Boot/App/App/src/test/java/com/baeldung/protobuf/App/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.protobuf.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
